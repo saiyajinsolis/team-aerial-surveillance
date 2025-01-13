61835d1b0a87da7fb1e337501603d046973f874b
msolis-ICTC <msolis@student.monroecc.edu>
2025-01-13 08:02:34 -0500

    Debugged the surveilance program to work with the tello drone. Creating the resources folder within the directory before running the program allowed it to save the images to it since the program itself has no code that can create a directory within it.

--- DroneDataCollect.py
+++ DroneDataCollect.py
@@ -3,10 +3,15 @@ import KeyPressModule as kp
 import cv2
 import time
 
+testConnection = False
+controlDrone = True
+
 kp.init()
 me = tello.Tello()
 me.connect()
+me.streamon()
 print(me.get_battery())
+global img
 
 def getKeyboardInput():
     lr, fb, ud, yv = 0, 0, 0, 0
@@ -22,13 +27,16 @@ def getKeyboardInput():
     elif kp.getKey("s"): ud = -speed
 
     if kp.getKey("a"): yv = speed
-    elif kp.getKey("d"): yv = -speed
+    elif kp.getKey("d"): yv = -speedI
 
-    if kp.getKey("q"): yv = me.land()
-    elif kp.getKey("e"): yv = me.takeoff()
+    if kp.getKey("q"): me.land()
+    elif kp.getKey("e"): me.takeoff()
 
-    if kp.getKey('z'):
-        cv2.imwrite(f'Resources/Images/{time.time()}.jpg', img)
+    print(f"Current Speed: {speed}")
+
+    if kp.getKey('p'):
+        print("Image Captured")
+        cv2.imwrite(f'./Resources/Images/{time.time()}.jpg', img)
         time.sleep(0.3)
 
     return [lr, fb, ud, yv]
@@ -37,39 +45,49 @@ def handScan():
     speed = 15
 
     me.send_rc_control(speed, 0, 0, 0)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(0,0,0, speed)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(-speed, 0, 0, 0)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(0, 0, 0, -speed)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(-speed, 0, 0, 0)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(-speed, 0, 0, 0)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(0, 0, 0, -speed)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(-speed, 0, 0, 0)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(speed, 0, 0, 0)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(0, 0, 0, speed)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(speed, 0, 0, 0)
-    sleep(2)
+    time.sleep(2)
     me.send_rc_control(0, 0, 0, speed)
-    sleep(2)
+    time.sleep(2)
 
-while True:
+while controlDrone:
     vals = getKeyboardInput()
-    me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
-    if kp.getKey("g"): handScan()
-
     img = me.get_frame_read().frame
     img = cv2.resize(img, (360, 240))
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    
+    if kp.getKey("b"): 
+        testConnection = True
+
+    while testConnection:
+        print(me.get_battery())
+        if kp.getKey("b"): 
+            testConnection = False
+
+    # if kp.getKey("g"): handScan()
+
+   
+    # cv2.imshow("Image", img)
+    # cv2.waitKey(1)w
 
 
 
--- KeyPressModule.py
+++ KeyPressModule.py
@@ -2,7 +2,7 @@ import pygame
 
 def init():
     pygame.init()
-    win = pygame.display.set_mode((400, 400))
+    win = pygame.display.set_mode((600, 400))
 
 def getKey(keyName):
     ans = False
@@ -20,6 +20,11 @@ def main():
         print("Left key pressed")
     if getKey("RIGHT"):
         print("Right key pressed")
+    if getKey("UP"):
+        print("UP key pressed")
+    if getKey("DOWN"):
+        print("DOWN key pressed")
+    
 
 
 if __name__ == '__main__':
